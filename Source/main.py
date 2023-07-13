fcdf5d8d069aedc8d3b44f9e7fea35c9a266f78e
Ronald Du <75736373+duronald@users.noreply.github.com>
2023-07-13 03:26:43 -0700

    Add files via upload

--- Source/main.py
+++ Source/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game:
     def __init__(self):
@@ -8,6 +9,7 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH,HEIGHT)) #creates display surface and a clock
         pygame.display.set_caption('RPG')
         self.clock = pygame.time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
@@ -17,6 +19,7 @@ class Game:
                     sys.exit()
 
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
