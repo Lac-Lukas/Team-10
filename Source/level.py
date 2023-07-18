fbeaf9a830288d66b44761607f4b46dfc577d80c
lukas <lukasl@nevada.unr.edu>
2023-07-17 19:37:58 -0700

    7/17/2023 added many features, updates listed on discord

--- Source/level.py
+++ Source/level.py
@@ -3,7 +3,6 @@ from settings import *
 from tile import Tile
 from player import Player
 
-
 class Level:
 	def __init__(self):
 
@@ -11,7 +10,7 @@ class Level:
 		self.display_surface = pygame.display.get_surface()
 
 		# sprite group setup
-		self.visible_sprites = pygame.sprite.Group()
+		self.visible_sprites = YSortCameraGroup()
 		self.obstacle_sprites = pygame.sprite.Group()
 
 		# sprite setup
@@ -25,9 +24,27 @@ class Level:
 				if col == 'x':
 					Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
 				if col == 'p':
-					Player((x,y),[self.visible_sprites], self.obstacle_sprites)
+					self.player = Player((x,y),[self.visible_sprites], self.obstacle_sprites)
 
 	def run(self):
 		# update and draw the game
-		self.visible_sprites.draw(self.display_surface)
+		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
+
+class YSortCameraGroup (pygame.sprite.Group):
+	def __init__(self):
+
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.half_width = self.display_surface.get_size()[0] // 2
+		self.half_height = self.display_surface.get_size()[1] // 2
+		self.offset = pygame.math.Vector2()
+
+	def custom_draw(self,player): #logic for the camera, overlaps sprites in the Y direction
+		#get offsets
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height
+
+		for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image, offset_pos)
--- Source/player.py
+++ Source/player.py
@@ -1,56 +1,65 @@
 import pygame
-from pygame.locals import *
 from settings import *
 
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self,pos,groups, obstacles):
+	def __init__(self,pos,groups, obstacle_sprites):
 		super().__init__(groups)
-		self.image = pygame.image.load('../graphics/player_right.png').convert_alpha()
-		self.rect = self.image.get_rect(topleft = pos)
-		self.display_surface = pygame.display.get_surface()
-		
-		self.pos_offset = [0, 0]
-		self.speed = 7
-		self.obstacles = obstacles
+		self.image = pygame.image.load('../graphics/_Idle.png').convert_alpha()
+		self.image = pygame.transform.scale(self.image, (64,80))
 
-	def get_mvmt(self):
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-30) #changes sprites hitbox
+
+		self.direction = pygame.math.Vector2()
+		self.speed = 7
+		self.obstacle_sprites = obstacle_sprites
+
+	def input(self):
 		#if no movement key is pressed, there should be no offset
-		self.pos_offset = [0, 0]
 		keys = pygame.key.get_pressed()
 
 		#check for horizontal movement
-		if keys[pygame.K_LEFT]:
-			self.pos_offset[0] = -1
-		if keys[pygame.K_RIGHT]:
-			self.pos_offset[0] += 1
+		if keys[pygame.K_LEFT] or keys[ord('a')]:
+			self.direction.x = -1
+		elif keys[pygame.K_RIGHT] or keys[ord('d')]:
+			self.direction.x = 1
+		else:
+			self.direction.x = 0
 		#check for vertical movement
-		if keys[pygame.K_UP]:
-			self.pos_offset[1] = -1
-		if keys[pygame.K_DOWN]:
-			self.pos_offset[1] += 1
+		if keys[pygame.K_UP] or keys[ord('w')]:
+			self.direction.y = -1
+		elif keys[pygame.K_DOWN] or keys[ord('s')]:
+			self.direction.y = 1
+		else:
+			self.direction.y = 0
 
-	def check_collision(self):
-		test_player = Rect(self.rect[:])	#makes shallow copy
+	def move(self, speed):
+		if self.direction.magnitude() != 0:
+			self.direction = self.direction.normalize()
 
-		#check if horizontal movement will cause collision
-		test_player[0] += (self.pos_offset[0] * self.speed)
-		for obstacle in self.obstacles:
-				if pygame.Rect.colliderect(obstacle.rect, test_player):
-					self.pos_offset[0] = 0
-					test_player[0] = self.rect[0]
-		#check if vertical movement will cause collision
-		test_player[1] += (self.pos_offset[1] * self.speed)
-		for obstacle in self.obstacles:
-				if pygame.Rect.colliderect(obstacle.rect, test_player):
-					self.pos_offset[1] = 0
+		self.hitbox.x += self.direction.x * speed
+		self.collision('horizontal')
+		self.hitbox.y += self.direction.y * speed
+		self.collision('vertical')
+		self.rect.center =self.hitbox.center
 
-	
-	def move_player(self):
-		self.rect[0] += (self.pos_offset[0] * self.speed)
-		self.rect[1] += (self.pos_offset[1] * self.speed)
-
+	def collision(self,direction):
+		if direction == 'horizontal':
+			for sprite in self.obstacle_sprites:
+				if sprite.hitbox.colliderect(self.hitbox):
+					if self.direction.x > 0: # moving right
+						self.hitbox.right = sprite.hitbox.left
+					if self.direction.x < 0: # moving left
+						self.hitbox.left = sprite.hitbox.right	
+			
+		if direction == 'vertical':
+			for sprite in self.obstacle_sprites:
+				if sprite.hitbox.colliderect(self.hitbox):
+					if self.direction.y > 0: # moving down
+						self.hitbox.bottom = sprite.hitbox.top
+					if self.direction.y < 0: # moving up
+						self.hitbox.top = sprite.hitbox.bottom	
 	def update(self):
-		self.get_mvmt()
-		self.check_collision()
-		self.move_player()
+		self.input()
+		self.move(self.speed)
