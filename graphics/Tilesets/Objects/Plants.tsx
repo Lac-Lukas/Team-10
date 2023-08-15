ac4e19875e1eae10fde5ec74c6c9f6fac4bd1f48
lukas <lukasl@nevada.unr.edu>
2023-08-15 14:00:58 -0700

    committed only source before

--- graphics/Tilesets/Objects/Plants.tsx
+++ graphics/Tilesets/Objects/Plants.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Plants" tilewidth="224" tileheight="272" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="Plants" tilewidth="224" tileheight="278" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="224" height="272" source="Plants/Tree.png"/>
+ </tile>
+ <tile id="1">
+  <image width="191" height="278" source="Plants/Tree (2).png"/>
  </tile>
 </tileset>
